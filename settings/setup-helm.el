a3567652fa4d25a03b0d355074ed8fb10501252b
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-09-21 10:25:25 +0300

    Init helm prefix command

--- settings/setup-helm.el
+++ settings/setup-helm.el
@@ -33,10 +33,20 @@
 
 (helm-mode 1)
 
+(define-key global-map [remap find-file] 'helm-find-files)
+(define-key global-map [remap occur] 'helm-occur)
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+(define-key global-map [remap execute-extended-command] 'helm-M-x)
+(unless (boundp 'completion-in-region-function)
+  (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+  (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
+
 ;; set up helm projectile
 (require 'helm-projectile)
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (provide 'setup-helm)
