09428228395cc70b35849551f1a61187eb88e488
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-09-22 23:49:40 +0300

    Add multiple cursors using mouse click

--- init.el
+++ init.el
@@ -112,6 +112,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
 ;; autocomplete
 (require 'auto-complete-config)
