37ac4480273c7c56b26a8f6102df8befe2290cae
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-09-22 23:48:11 +0300

    add multiple cursors

--- init.el
+++ init.el
@@ -106,6 +106,13 @@
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
+;; setting up multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 ;; autocomplete
 (require 'auto-complete-config)
 (ac-config-default)
