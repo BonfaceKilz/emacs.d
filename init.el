fdfb61f906df760b334312951e1cecb1f79ffc06
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-09-22 22:36:13 +0300

    Rearrange things

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 ;; Set up a settings dir
 (setq settings-dir
       (expand-file-name "settings" user-emacs-directory))
+(add-to-list 'load-path settings-dir)
 
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
@@ -25,6 +26,7 @@
 ;; Set path to dependencies
 (setq site-lisp-dir
       (expand-file-name "site-lisp" user-emacs-directory))
+(add-to-list 'load-path site-lisp-dir)
 
 ;; Set up vendor dir
 (defvar bonface/vendor-dir (expand-file-name "vendor" user-emacs-directory))
@@ -43,11 +45,6 @@
 (require 'saveplace)
 (setq-default save-place t)
 (setq save-place-file (expand-file-name ".places" user-emacs-directory))
-
-(add-to-list 'load-path settings-dir)
-(add-to-list 'load-path site-lisp-dir)
-(add-to-list 'load-path "~/.emacs.d/elpa/xelb-0.15")
-(add-to-list 'load-path "~/.emacs.d/elpa/exwm-0.19")
 
 (require 'setup-init)
 (require 'setup-envs)
