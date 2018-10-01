06bfc5e534b6f7a145a4587777873058b75b612c
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-10-01 23:18:45 +0300

    Add systemtray and randr to exwm config

--- settings/setup-exwm.el
+++ settings/setup-exwm.el
@@ -3,4 +3,21 @@
 (require 'exwm-config)
 (exwm-config-default)
 
+(require 'exwm-systemtray)
+(exwm-systemtray-enable)
+
+(require 'exwm-randr)
+(setq exwm-randr-workspace-output-plist '(0 "eDP-1"
+					    1 "eDP-1"
+					    2 "HDMI-1"
+					    3 "HDMI-1")
+      )
+(add-hook 'exwm-randr-screen-change-hook
+          (lambda ()
+            (start-process-shell-command
+             "xrandr" nil "xrandr --output eDP-1 --right-of HDMI-1 --auto")))
+(exwm-randr-enable)
+
+(exwm-randr-enable)
+
 (provide 'setup-exwm)
