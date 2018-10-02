ba557068cfc875b86aaec2832d9021cfe97cfde4
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-10-02 23:37:58 +0300

    Remove text-scale fns
    Just realised that I can't use -ve funtions which is really annoying

--- settings/my-misc.el
+++ settings/my-misc.el
@@ -2,8 +2,6 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C-;") 'comment-or-uncomment-region)
 (global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-c-+") 'text-scale-increase)
-(global-set-key (kbd "C-c-") 'text-scale-decrease)
 (global-set-key (kbd "C-c C-k") 'compile)
 (global-set-key (kbd "C-x g") 'magit-status)
 
