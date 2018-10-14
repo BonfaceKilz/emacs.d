70053e826742967bdc0d7e119f9ae69c9c68082e
BonfaceKilz <bonfacemunyoki@gmail.com>
2018-10-14 21:52:32 +0300

    Correct typo

--- settings/setup-helm.el
+++ settings/setup-helm.el
@@ -10,7 +10,7 @@
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
 
-(setq helm-split-window-in-sIde-P           t ; open helm buffer inside current window, not occupy whole other window
+(setq helm-split-window-in-side-P           t ; open helm buffer inside current window, not occupy whole other window
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
@@ -52,4 +52,4 @@
 
 (require 'helm-exwm)
 (global-set-key (kbd "C-c b") 'helm-exwm)
-(provide 'setup-helm)
+(provide 'setup-helm) 
