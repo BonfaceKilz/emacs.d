7051774e389d3efabaf294206ceb42f32ba453f2
BonfaceKilz <bonfacemunyoki@gmail.com>
2019-01-04 18:05:34 +0300

    Add new configs

---
+++ settings/default-packages.el
@@ -0,0 +1,86 @@
+;; Setup default packages
+
+(defvar bonface/packages '(ac-slime
+                           all-the-icons
+                           auto-complete
+                           autopair
+                           avy
+                           better-defaults
+                           buffer-move
+                           change-inner
+                           dash
+                           desktop-environment
+                           diminish
+                           diff-hl
+                           elpy
+                           epresent
+                           eproject
+                           ess
+                           eshell-autojump
+                           expand-region
+                           f
+                           fill-column-indicator
+                           feature-mode
+                           flycheck
+                           focus
+                           gist
+                           go-autocomplete
+                           go-eldoc
+                           go-mode
+                           golden-ratio
+                           guide-key
+                           graphviz-dot-mode
+                           haml-mode
+                           haskell-mode
+                           helm-exwm
+                           helm-projectile
+                           highlight-escape-sequences
+                           highlight-symbol
+                           htmlize
+                           magit
+                           magit-org-todos
+                           markdown-mode
+                           marmalade
+                           move-text
+                           multiple-cursors
+                           neotree
+                           nodejs-repl
+                           org
+                           paredit
+                           php-mode
+                           php-refactor-mode
+                           powerline
+                           prodigy
+                           persp-projectile
+                           projectile
+                           py-autopep8
+                           rvm
+                           s
+                           smart-forward
+                           smartparens
+                           string-edit
+                           switch-window
+                           use-package
+                           undo-tree
+                           visual-regexp
+                           web-mode
+                           writegood-mode
+                           yaml-mode
+                           yasnippet
+                           yasnippet-snippets
+                           zoom-window)
+  "Default packages")
+
+(defun bonface/packages-installed-p ()
+  (loop for pkg in bonface/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (bonface/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg bonface/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+(provide 'setup-default-packages)
---
+++ settings/misc.el
@@ -0,0 +1,14 @@
+;; My misc key bindings
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-c C-k") 'compile)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-c d") 'insert-standard-date)
+
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+(show-paren-mode t)
+
+(provide 'my-misc)
