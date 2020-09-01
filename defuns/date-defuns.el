850b77bafbb8622ff63076f4062f26566b1432ab
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-09-01 22:00:18 +0300

    Delete files
    All the deleted files can be found inside emacs.org file

--- defuns/date-defuns.el
+++
@@ -1,4 +0,0 @@
-(defun insert-standard-date ()
-  "Inserts standard date time string."
-  (interactive)
-  (insert (format-time-string "%c")))
--- settings/setup-hugo.el
+++
@@ -1,8 +0,0 @@
-(setq easy-hugo-basedir "~/Public/BonfaceKilz/")
-(setq easy-hugo-url "https://bonfacemunyoki.com")
-(setq easy-hugo-sshdomain "myserver")
-(setq easy-hugo-root "/home/bonface/bonfacemunyoki.com/")
-(setq easy-hugo-previewtime "300")
-(define-key global-map (kbd "C-c C-e") 'easy-hugo)
-
-(provide 'setup-hugo)
